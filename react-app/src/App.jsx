4271ecad2fe20ea0628cf8e02a8a7d5d586c72e1
Benkolon152 <v.benasz@gmail.com>
2025-09-25 08:04:58 +0200

    Button

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default class App extends React.Component{
+  state = {
+    cntChildren: 3
+  }
 
-  return (
-    <>
+  handleAppendChildClick = buttonId => this.setState({cntChildren: this.state.cntChildren+1})
+
+
+  render(){
+    return (
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          <button id='btnAppendChild' onClick={()=>this.handleAppendChildClick("btnAppendChild")}>Append child</button>
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    )
+  }
 }
-
-export default App
